bdc460ae1e80314b205ffff5ba465d9bae4bd66a
e-doschechnikova <evadrobakova@gmail.com>
2022-04-21 21:24:45 +0300

    add header, navigation, styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React from "react";
 import "./App.css";
-import Header from "./header";
-import Technologies from "./technologies";
-import Footer from "./footer";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://psv4.userapi.com/c237231/u37221233/docs/d31/57b7708cb613/logo-lama.png?extra=wfrJ9DFTtLooJe7kO-WSzq59fMMjq-qXKA6rcD4yS0aV1D8gXPu7AkP_LOxo6BdWK4K_-BQ0v1o0_AZu92JgnvGxPq9FXZKrDCd7lJWzx6PirLC06WEHwNGWtPQncua8I2tQbCPbZjEtf8NvrA"></img>
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Setting</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://afisha.london/wp-content/uploads/2020/07/alpacas-colour-pattern-1588x840-1.jpg"></img>
+        </div>
+        <div>
+          ava+ descr
+          <img src="https://sun9-24.userapi.com/s/v1/ig2/zAZ7I0Md_UB8Tu70q-7yAyM53SSJwDf7Vv_rNlBU9ONFYjbfAVITsf0H_xcHSs3SMspulTfUday_lJmxOrQhv2zC.jpg?size=720x720&quality=95&type=album"></img>
+        </div>
+        <div>
+          my post
+          <div>new post</div>
+        </div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
     </div>
   );
 };
