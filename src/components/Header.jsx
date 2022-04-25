b5454f8eeabab567d05f23cf39c4faa49f82ca99
e-doschechnikova <evadrobakova@gmail.com>
2022-04-25 19:38:23 +0300

    add modules for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Header.css";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={styles.header}>
       <img
         src="https://psv4.userapi.com/c237231/u37221233/docs/d31/57b7708cb613/logo-lama.png?extra=wfrJ9DFTtLooJe7kO-WSzq59fMMjq-qXKA6rcD4yS0aV1D8gXPu7AkP_LOxo6BdWK4K_-BQ0v1o0_AZu92JgnvGxPq9FXZKrDCd7lJWzx6PirLC06WEHwNGWtPQncua8I2tQbCPbZjEtf8NvrA"
         alt="logo"
