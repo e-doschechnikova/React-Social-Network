87ec7b5625236b1a3e71bfd0c536de9d7e00ecf4
e-doschechnikova <evadrobakova@gmail.com>
2022-04-25 19:10:29 +0300

    add new class for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import "./Navbar.css";
+import classes from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <nav className="nav">
-      <div className="item">
+    <nav className="Navbar_nav__P8IDh">
+      <div className="Navbar_item__0wJa1">
         <a href="#s">Profile</a>
       </div>
-      <div className="item">
+      <div className="Navbar_item__0wJa1">
         <a href="#s">Messages</a>
       </div>
-      <div className="item">
+      <div className="Navbar_item__0wJa1">
         <a href="#s">News</a>
       </div>
-      <div className="item">
+      <div className="Navbar_item__0wJa1">
         <a href="#s">Music</a>
       </div>
-      <div className="item">
+      <div className="Navbar_item__0wJa1">
         <a href="#s">Setting</a>
       </div>
     </nav>
