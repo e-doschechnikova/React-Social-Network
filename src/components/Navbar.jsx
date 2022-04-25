739cba673bf4142655dd355ac863bd7b5f75313c
e-doschechnikova <evadrobakova@gmail.com>
2022-04-25 19:29:48 +0300

    add modules, new classes for Profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import classes from "./Navbar.module.css";
+import styles from "./Navbar.module.css";
+
+// let styles = {
+//   nav: "Navbar_nav__P8IDh",
+//   item: "Navbar_item__0wJa1",
+// };
 
 const Navbar = () => {
   return (
-    <nav className="Navbar_nav__P8IDh">
-      <div className="Navbar_item__0wJa1">
+    <nav className={styles.nav}>
+      <div className={styles.item}>
         <a href="#s">Profile</a>
       </div>
-      <div className="Navbar_item__0wJa1">
+      <div className={styles.item}>
         <a href="#s">Messages</a>
       </div>
-      <div className="Navbar_item__0wJa1">
+      <div className={styles.item}>
         <a href="#s">News</a>
       </div>
-      <div className="Navbar_item__0wJa1">
+      <div className={styles.item}>
         <a href="#s">Music</a>
       </div>
-      <div className="Navbar_item__0wJa1">
+      <div className={styles.item}>
         <a href="#s">Setting</a>
       </div>
     </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import "./Profile.css";
+import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
+import styles from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={styles.content}>
       <div>
         <img
           src="https://afisha.london/wp-content/uploads/2020/07/alpacas-colour-pattern-1588x840-1.jpg"
@@ -21,8 +22,8 @@ const Profile = () => {
         my post
         <div>new post</div>
       </div>
-      <div className="posts">
-        <div className="item">post1</div>
+      <div className={styles.posts}>
+        <div className={styles.item}>post1</div>
         <div className="item">post2</div>
       </div>
     </div>
