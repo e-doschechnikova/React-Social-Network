ce73ea101cadd8f9e2ec1b35e80410aff0532e4f
e-doschechnikova <evadrobakova@gmail.com>
2022-04-28 21:54:16 +0300

    add NavLink for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogsItems}>
-        <div className={styles.dialog + " " + styles.active}>Lama</div>
-        <div className={styles.dialog}>Lama Husband</div>
-        <div className={styles.dialog}>Lama Mama</div>
-        <div className={styles.dialog}>Lama Papa</div>
-        <div className={styles.dialog}>Lama Sister</div>
-        <div className={styles.dialog}>Lama Ira</div>
+        <div className={styles.dialog + " " + styles.active}>
+          <NavLink to="/dialogs/1">Lama</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/2">Lama Husband</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/3">Lama Mama</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/4">Lama Papa</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/5">Lama Sister</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/6">Lama Ira</NavLink>
+        </div>
       </div>
       <div className={styles.messages}>
         <div className={styles.message}>Hi!</div>
