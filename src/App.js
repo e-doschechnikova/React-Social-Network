9231c7ac589e6d664dfc9d7c08c00239846b2505
e-doschechnikova <evadrobakova@gmail.com>
2022-04-28 15:00:31 +0300

    add components and routes News, Music, Setting

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
+import Music from "./components/Music/Music";
 import Navbar from "./components/Navbar/Navbar";
+import News from "./components/News/News";
 import Profile from "./components/Profile/Profile";
+import Setting from "./components/Setting/Setting";
 
 const App = (props) => {
   return (
@@ -16,6 +19,9 @@ const App = (props) => {
           <Routes>
             <Route path="/dialogs" element={<Dialogs />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/setting" element={<Setting />} />
           </Routes>
         </div>
       </div>
---
+++ src/components/Setting/Setting.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Setting.module.css";
+
+const Setting = (props) => {
+  return <div>Setting</div>;
+};
+
+export default Setting;
