8730f7fb35eabc626c6b52f616e49f2919754680
e-doschechnikova <evadrobakova@gmail.com>
2022-04-29 20:18:03 +0300

    add arr and components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,7 +16,7 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
-  let dialogsData = [
+  let dialogs = [
     { id: 1, name: "Lama" },
     { id: 2, name: "Lama Husband" },
     { id: 3, name: "Lama Mama" },
@@ -25,7 +25,7 @@ const Dialogs = (props) => {
     { id: 6, name: "Lama Egor" },
   ];
 
-  let messagesData = [
+  let messages = [
     { id: 1, message: "Hi!" },
     { id: 2, message: "Where are you?" },
     { id: 3, message: "OK!" },
@@ -34,24 +34,15 @@ const Dialogs = (props) => {
     { id: 6, message: "Bye!" },
   ];
 
+  let dialogsElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+  let messagesElements = messages.map((m) => <Message message={m.message} />);
+
   return (
     <div className={styles.dialogs}>
-      <div className={styles.dialogsItems}>
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-        <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-        <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
-      </div>
-      <div className={styles.messages}>
-        <Message message={messagesData[0].message} />
-        <Message message={messagesData[1].message} />
-        <Message message={messagesData[2].message} />
-        <Message message={messagesData[3].message} />
-        <Message message={messagesData[4].message} />
-        <Message message={messagesData[5].message} />
-      </div>
+      <div className={styles.dialogsItems}>{dialogsElements}</div>
+      <div className={styles.messages}>{messagesElements}</div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,14 +3,16 @@ import styles from "../MyPosts/MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-  let postData = [
+  let posts = [
     { id: 1, message: "Hi, how are you?", likesCount: 1 },
     { id: 2, message: "It's my first post!", likesCount: 12 },
     { id: 3, message: "Oh, no! I'm lost!", likesCount: 6 },
     { id: 4, message: "Where am I?", likesCount: 60 },
   ];
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
-  console.log(postData);
   return (
     <div className={styles.postsBlock}>
       <h3>My post</h3>
@@ -22,24 +24,7 @@ const MyPosts = (props) => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={styles.posts}>
-        <Post
-          message={postData[0].message}
-          likesCount={postData[0].likesCount}
-        />
-        <Post
-          message={postData[1].message}
-          likesCount={postData[1].likesCount}
-        />
-        <Post
-          message={postData[2].message}
-          likesCount={postData[2].likesCount}
-        />
-        <Post
-          message={postData[3].message}
-          likesCount={postData[3].likesCount}
-        />
-      </div>
+      <div className={styles.posts}>{postsElements}</div>
     </div>
   );
 };
