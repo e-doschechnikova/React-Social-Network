df44e4ec82d5c8aaf1493924dd212e15d055df59
e-doschechnikova <evadrobakova@gmail.com>
2022-05-15 12:05:00 +0300

    fix logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,7 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img
-        src="https://psv4.userapi.com/c236331/u37221233/docs/d20/6e71742d302f/lama-lineyny-kontseptsia-simvol-znak-vektor-znachok-linia-kontur-izobrazhenie_csp69231674-removebg-preview.png?extra=ubRfORbsKawwbiavHZUzu6cAtswZ7zy8XsHajGQ62-sW6tqOf6aw7HLFNejpQOgEDJwckxbCqL_-RHqjD2WLWOaFQ4dr8ioFPyoUxc7cL1Day1uIVCt3ZcfIVK6VCrckPslJGYfFNi8eazuGUS0"
-        alt="logo"
-      ></img>
+      <img src="../../img/logo.png" alt="logo"></img>
     </header>
   );
 };
