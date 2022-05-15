526be48143cd374c9508644c62c96d0c04e40cc0
e-doschechnikova <evadrobakova@gmail.com>
2022-05-15 12:36:50 +0300

    add favicon and fix header logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Header.module.css";
+import logo from "../../img/logo.png";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img src="../../img/logo.png" alt="logo"></img>
+      <img src={logo} alt="logo" />
     </header>
   );
 };
