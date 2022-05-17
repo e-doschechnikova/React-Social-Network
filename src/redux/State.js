8cc4a5691e57e5ae72c2cfa8bdd9a6a6773cb72e
e-doschechnikova <evadrobakova@gmail.com>
2022-05-17 16:08:31 +0300

    refactoring State and MyPost

--- src/redux/State.js
+++ src/redux/State.js
@@ -114,22 +114,6 @@ let store = {
     this._callSubscriber = observer;
   },
 
-  addPost() {
-    let newPost = {
-      id: 5,
-      message: this._state.profilePage.newPostText,
-      likesCount: 0,
-    };
-
-    this._state.profilePage.posts.push(newPost);
-    this._state.profilePage.newPostText = "";
-    this._callSubscriber(this._state);
-  },
-  updateNewPostText(newText) {
-    this._state.profilePage.newPostText = newText;
-    this._callSubscriber(this._state);
-  },
-
   dispatch(action) {
     //{type: "ADD-POST"}
     if (action.type === "ADD-POST") {
